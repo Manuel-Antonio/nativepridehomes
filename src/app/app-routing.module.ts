d46c53f600519ec87d82b7db4b388776014bf087
Manuel Huaman <manuelhuaman1998@gmail.com>
2024-06-24 15:09:20 -0500

    Creando paginas demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: HeroComponent },
+  { path: 'servicios', component: ServicesComponent },
+  { path: 'proyectos', component: ProjectsComponent },
+  { path: 'testimonios', component: TestimonialsComponent },
+  { path: 'contacto', component: ContactComponent },
+  { path: '**', redirectTo: '/inicio' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
